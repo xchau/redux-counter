cb5754db2f4a322db710e8e57e00406cf90e5583
Minh Chau <xiu.chau@gmail.com>
2017-07-28 16:20:10 -0700

    create only slim components

--- src/Counter.js
+++ src/Counter.js
@@ -1,21 +1,7 @@
 import React, { Component } from 'react';
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>hi</div>
-    );
-  }
+export const Counter = (props) => {
+  return (
+    <div>hi</div>
+  );
 };
-
-export default Counter;
-
-// export const Counter = (props) => {
-//   return (
-//     <div>hi</div>
-//   );
-// };
